b0b0efa4dbb77e2ac7146f6c804331491eddc25a
undefinedyobra <brianmelly008@gmail.com>
2024-04-17 18:45:47 +0300

    code

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 // Function to fetch dog breeds from db.json
 async function fetchDogBreeds() {
-    const response = await fetch('http://localhost:3000/');
+    const response = await fetch('https://json-server-7-hi6u.onrender.com');
     const data = await response.json();
     return data.dogBreeds;
 }
